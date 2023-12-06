cbb44412cccbf116871b4fd5139619519fd9ea68
subhikshaganesan98 <subhikshaganesan@yahoo.com>
2023-12-05 23:14:06 -0500

    User Points increment and decrement for both rider and driver completed

--- app/src/main/java/edu/uga/cs/ridingapp/AcceptedRequestsRecyclerAdapter.java
+++ app/src/main/java/edu/uga/cs/ridingapp/AcceptedRequestsRecyclerAdapter.java
@@ -99,69 +99,12 @@ public class AcceptedRequestsRecyclerAdapter extends RecyclerView.Adapter<Accept
 
                 if (currentUser != null) {
                     // Get the current user's email
-                    String currentEmail = currentUser.getEmail();
+                    String currentRiderEmail = currentUser.getEmail();
+                    String currentDriverEmail = acceptedRequest.getDriverName(); // Replace with the actual way to get the rider email
 
-                    // Query the database to find the user with the matching email
-                    reference.orderByChild("email").equalTo(currentEmail).addListenerForSingleValueEvent(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                            if (dataSnapshot.exists()) {
-                                // Loop through the results (there should be only one match)
-                                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                                    // Get the userPoints snapshot
-                                    DataSnapshot userPointsSnapshot = snapshot.child("userPoints");
-
-                                    // Check if the "userPoints" node exists
-                                    if (userPointsSnapshot.exists()) {
-                                        // Get the current userPoints value
-                                        Object rawUserPoints = userPointsSnapshot.getValue();
-
-                                        // Check if rawUserPoints is not null
-                                        if (rawUserPoints != null) {
-                                            // Attempt to convert the value to Integer
-                                            try {
-                                                Integer existingUserPoints = Integer.valueOf(rawUserPoints.toString());
-
-                                                // Add 50 to the existing userPoints
-                                                int updatedUserPoints = existingUserPoints - 50;
-
-                                                // Update the userPoints in the database
-                                                reference.child(snapshot.getKey()).child("userPoints").setValue(updatedUserPoints);
-
-                                                // Access the context from the adapter
-                                                if (context != null) {
-                                                    Toast.makeText(context, "UserPoints are updated", Toast.LENGTH_SHORT).show();
-                                                } else {
-                                                    Log.e("TAG", "Context is null");
-                                                }
-                                            } catch (NumberFormatException e) {
-                                                // Handle the case where the "userPoints" value cannot be converted to Integer
-                                                Log.e("TAG", "Error converting userPoints to Integer: " + e.getMessage());
-                                            }
-                                        } else {
-                                            // Handle the case where rawUserPoints is null
-                                            // This may happen if the "userPoints" value is missing or null in the database
-                                            Log.e("TAG", "userPoints value is missing or null in the database");
-                                        }
-                                    } else {
-                                        // Handle the case where the "userPoints" node does not exist in the database
-                                        Log.e("TAG", "userPoints node does not exist in the database");
-                                    }
-                                }
-                            } else {
-                                // No user found with the current user's email
-                                if (context != null) {
-                                    Toast.makeText(context, "No user found with the current user's email", Toast.LENGTH_SHORT).show();
-                                } else {
-                                    Log.e("TAG", "Context is null");
-                                }
-                            }
-                        }
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError databaseError) {
-                            Log.e("TAG", "Database error: " + databaseError.getMessage());
-                        }
-                    });
+                    // Function to update user points based on the email
+                    updateUserPoints(currentDriverEmail, 50);
+                    updateUserPoints(currentRiderEmail, -50);
                 } else {
                     // User is not signed in
                     if (context != null) {
@@ -170,6 +113,69 @@ public class AcceptedRequestsRecyclerAdapter extends RecyclerView.Adapter<Accept
                         Log.e("TAG", "Context is null");
                     }
                 }
+            }
+        });
+    }
+
+    // Separate function to update user points
+    private void updateUserPoints(String userEmail, int pointsChange) {
+        // Query the database to find the user with the matching email
+        reference.orderByChild("email").equalTo(userEmail).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    // Loop through the results (there should be only one match)
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        // Get the userPoints snapshot
+                        DataSnapshot userPointsSnapshot = snapshot.child("userPoints");
+
+                        // Check if the "userPoints" node exists
+                        if (userPointsSnapshot.exists()) {
+                            // Get the current userPoints value
+                            Object rawUserPoints = userPointsSnapshot.getValue();
+
+                            // Check if rawUserPoints is not null
+                            if (rawUserPoints != null) {
+                                // Attempt to convert the value to Integer
+                                try {
+                                    Integer existingUserPoints = Integer.valueOf(rawUserPoints.toString());
+
+                                    // Update the userPoints based on the pointsChange
+                                    int updatedUserPoints = existingUserPoints + pointsChange;
+
+                                    // Update the userPoints in the database
+                                    reference.child(snapshot.getKey()).child("userPoints").setValue(updatedUserPoints);
+
+                                    // Access the context from the adapter
+                                    if (context != null) {
+                                        Toast.makeText(context, "UserPoints are updated", Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        Log.e("TAG", "Context is null");
+                                    }
+                                } catch (NumberFormatException e) {
+                                    // Handle the case where the "userPoints" value cannot be converted to Integer
+                                    Log.e("TAG", "Error converting userPoints to Integer: " + e.getMessage());
+                                }
+                            } else {
+                                // Handle the case where rawUserPoints is null
+                                // This may happen if the "userPoints" value is missing or null in the database
+                                Log.e("TAG", "userPoints value is missing or null in the database");
+                            }
+                        } else {
+                            // Handle the case where the "userPoints" node does not exist in the database
+                            Log.e("TAG", "userPoints node does not exist in the database");
+                        }
+                    }
+                } else {
+                    // No user found with the provided email
+                    Log.d("TAG", "No user found with the provided email: " + userEmail);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Handle the error
+                Log.e("TAG", "Database error: " + databaseError.getMessage());
             }
         });
     }
